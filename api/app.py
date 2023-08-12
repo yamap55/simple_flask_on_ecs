29f8c952d7f3a974c5923cf84957e1f3daefa4bc
yamap55 <1785817+yamap55@users.noreply.github.com>
2023-08-12 10:56:22 +0900

    add: simple flask app

---
+++ api/app.py
@@ -0,0 +1,26 @@
+"""main"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """hello world"""
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/getTasks")
+def get_tasks():
+    """get tasks"""
+    tasks = [
+        {
+            "id": 1,
+            "name": "task1",
+        },
+        {
+            "id": 2,
+            "name": "task2",
+        },
+    ]
+    return jsonify(tasks)
